6650a9ef6c43e3afc741e6e4624b04cef45af8cf
Paul Karayan <paulkarayan@gmail.com>
2019-09-16 13:30:31 -0700

    add cosmic badge for free bucket

--- layouts/default.vue
+++ layouts/default.vue
@@ -22,6 +22,7 @@
       <div class="container">
         <div class="content has-text-centered">
           Proudly Powered By <a href="https://cosmicjs.com" class="has-text-dark"><strong>Cosmic JS</strong></a>
+          <a href="https://cosmicjs.com/add-bucket?import_bucket=5d7feba607e621480c42770b"><img src="https://cdn.cosmicjs.com/51fe54d0-4f6e-11e9-9f32-8d001da69630-powered-by-cosmicjs.svg" /></a>
         </div>
       </div>
     </footer>
